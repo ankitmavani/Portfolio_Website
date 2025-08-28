b2f72094b89b8ef92bfe70686fb8b5f53da9f348
ankitmavani <mavaniankit67@gmail.com>
2025-08-28 23:17:00 +0530

    setup blog page

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -3,35 +3,177 @@ import { ChevronRight, ChevronDown } from "lucide-react";
 
 const treeData = [
   {
-    name: "src",
+    name: "DSA",
     type: "folder",
     children: [
       {
-        name: "components",
+        name: "Array",
+        type: "folder",
+        children: [{ name: "Array.md", type: "file" }],
+      },
+      {
+        name: "String",
+        type: "folder",
+        children: [{ name: "String.md", type: "file" }],
+      },
+      {
+        name: "Object",
+        type: "folder",
+        children: [{ name: "Object.md", type: "file" }],
+      },
+      {
+        name: "Stack",
+        type: "folder",
+        children: [{ name: "Stack.md", type: "file" }],
+      },
+      {
+        name: "Queue",
+        type: "folder",
+        children: [{ name: "Queue.md", type: "file" }],
+      },
+      {
+        name: "Linked List",
+        type: "folder",
+        children: [{ name: "LinkedList.md", type: "file" }],
+      },
+      {
+        name: "HashMap & Hashing",
         type: "folder",
         children: [
-          { name: "Header.jsx", type: "file" },
-          { name: "Footer.jsx", type: "file" },
+          { name: "HashMap.md", type: "file" },
+          { name: "Hashing.md", type: "file" },
         ],
       },
       {
-        name: "pages",
+        name: "Sorting & Searching",
         type: "folder",
         children: [
-          { name: "Home.jsx", type: "file" },
-          { name: "About.jsx", type: "file" },
+          { name: "Sorting.md", type: "file" },
+          { name: "Searching.md", type: "file" },
         ],
       },
-      { name: "App.js", type: "file" },
-      { name: "index.js", type: "file" },
+      {
+        name: "Trees",
+        type: "folder",
+        children: [{ name: "Trees.md", type: "file" }],
+      },
+      {
+        name: "Graphs",
+        type: "folder",
+        children: [{ name: "Graphs.md", type: "file" }],
+      },
+      {
+        name: "Divide and Conquer",
+        type: "folder",
+        children: [{ name: "DivideAndConquer.md", type: "file" }],
+      },
+      {
+        name: "Time Complexity",
+        type: "folder",
+        children: [{ name: "TimeComplexity.md", type: "file" }],
+      },
+      {
+        name: "Greedy Algo",
+        type: "folder",
+        children: [{ name: "Greedy.md", type: "file" }],
+      },
+      {
+        name: "Dynamic Programming",
+        type: "folder",
+        children: [{ name: "DynamicProgramming.md", type: "file" }],
+      },
+      {
+        name: "Bitwise",
+        type: "folder",
+        children: [{ name: "Bitwise.md", type: "file" }],
+      },
+      {
+        name: "Math",
+        type: "folder",
+        children: [{ name: "Math.md", type: "file" }],
+      },
+      {
+        name: "Backtracking",
+        type: "folder",
+        children: [{ name: "Backtracking.md", type: "file" }],
+      },
+      {
+        name: "Recursion",
+        type: "folder",
+        children: [{ name: "Recursion.md", type: "file" }],
+      },
+      {
+        name: "Heap",
+        type: "folder",
+        children: [{ name: "Heap.md", type: "file" }],
+      },
     ],
   },
   {
-    name: "public",
+    name: "HTML",
     type: "folder",
-    children: [{ name: "index.html", type: "file" }],
+    children: [{ name: "html.md", type: "file" }],
   },
-  { name: "package.json", type: "file" },
+  {
+    name: "CSS",
+    type: "folder",
+    children: [{ name: "css.md", type: "file" }],
+  },
+  {
+    name: "JavaScript",
+    type: "folder",
+    children: [{ name: "JavaScript.md", type: "file" }],
+  },
+  {
+    name: "TypeScript",
+    type: "folder",
+    children: [{ name: "TypeScript.md", type: "file" }],
+  },
+  {
+    name: "React",
+    type: "folder",
+    children: [{ name: "React.md", type: "file" }],
+  },
+  {
+    name: "NodeJS",
+    type: "folder",
+    children: [{ name: "Node.md", type: "file" }],
+  },
+  {
+    name: "SQL",
+    type: "folder",
+    children: [{ name: "SQL.md", type: "file" }],
+  },
+  {
+    name: "NoSQL",
+    type: "folder",
+    children: [{ name: "NoSQL.md", type: "file" }],
+  },
+  {
+    name: "Backend",
+    type: "folder",
+    children: [{ name: "backend.md", type: "file" }],
+  },
+  {
+    name: "Frontend",
+    type: "folder",
+    children: [{ name: "Frontend.md", type: "file" }],
+  },
+  {
+    name: "Git",
+    type: "folder",
+    children: [{ name: "Git.md", type: "file" }],
+  },
+  {
+    name: "Docker",
+    type: "folder",
+    children: [{ name: "Docker.md", type: "file" }],
+  },
+  {
+    name: "System Design",
+    type: "folder",
+    children: [{ name: "SystemDesign.md", type: "file" }],
+  },
 ];
 
 const TreeNode = ({ node, level = 0 }) => {
@@ -58,7 +200,7 @@ const TreeNode = ({ node, level = 0 }) => {
         )}
 
         {/* File/Folder Name */}
-        <span className="text-gray-200 text-2xl">{node.name}</span>
+        <span className="text-gray-200 text-sm">{node.name}</span>
       </div>
 
       {/* Children */}
@@ -77,7 +219,7 @@ export default function Blog() {
   return (
     <article className="blog active" data-page="about">
       <header>
-        <h2 className="h2 article-title">About me</h2>
+        <h2 className="h2 article-title">Blog</h2>
       </header>
       <div className="text-sm text-gray-200 font-mono">
         {treeData.map((node, index) => (
